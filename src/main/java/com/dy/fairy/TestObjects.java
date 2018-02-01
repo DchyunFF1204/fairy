326cc30317880752349334938a7e46d0e5c113d2
daizhaoyun <zdai@xiaohongshu.com>
2018-02-01 12:53:22 +0800

    Objects方法测试

---
+++ src/main/java/com/dy/fairy/TestObjects.java
@@ -0,0 +1,28 @@
+package com.dy.fairy;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class TestObjects {
+
+    public static void main(String[] args) {
+        System.out.println(Objects.requireNonNull("sss"));
+        System.out.println(Objects.isNull(null));
+        System.out.println(Objects.nonNull(null));
+        System.out.println(Objects.equals(1,1));
+        System.out.println(Objects.deepEquals(Integer.valueOf("1"),1L));
+        JSONObject jo = new JSONObject();
+        jo.put("key",2);
+        JSONObject jo1 = new JSONObject();
+        jo1.put("key",1);
+        int s = Objects.compare(jo, jo1, new Comparator<JSONObject>() {
+            @Override
+            public int compare(JSONObject o1, JSONObject o2) {
+                return o1.getInteger("key").compareTo(o2.getInteger("key"));
+            }
+        });
+        System.out.println(s);
+    }
+}
